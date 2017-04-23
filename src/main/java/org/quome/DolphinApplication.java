3c45494cfcf2f0dbd90fabc636683f1d4081c8d9
yujie0121 <yujie250212>
2017-04-23 15:53:26 +0800

    first commit

---
+++ src/main/java/org/quome/DolphinApplication.java
@@ -0,0 +1,12 @@
+package org.quome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolphinApplication.class, args);
+	}
+}
---
+++ src/test/java/org/quome/DolphinApplicationTests.java
@@ -0,0 +1,16 @@
+package org.quome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DolphinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
